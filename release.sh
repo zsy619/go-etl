3e62c3e6732e47cc6bd9b6fb560f0b583a872c6a
朱书彦 <zsy619@163.com>
2024-07-30 10:54:49 +0800

    【refactor】修订脚本

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh cover.sh
+# sh cover.sh
 
 export IGNORE_PACKAGES=db2
 export CGO_ENABLED=1
@@ -14,21 +14,21 @@ cd cmd/datax
 echo "start build darwin/amd64 ..."
 CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -o datax-darwin
 cp -f datax-darwin ../../release/datax-darwin
-chmod -R 777 ../../release/datax-darwin
+chmod +x ../../release/datax-darwin
 rm -f datax-darwin
 
 # 【linux/amd64】
 # echo "start build linux/amd64 ..."
 # CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o datax-linux
 # cp -f datax-linux ../../release/datax-linux
-# chmod -R 777 ../../release/datax-linux
+# chmod +x../../release/datax-linux
 # rm -f datax-linux
 
 # 【windows/amd64】
 echo "start build windows/amd64 ..."
 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 go build -v -o datax.exe
 cp -f datax.exe ../../release/datax.exe
-chmod -R 777 ../../release/datax.exe
+chmod +x ../../release/datax.exe
 rm -f datax.exe
 
 cd ../..
